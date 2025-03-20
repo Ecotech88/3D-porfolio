7c5824006bdb70044de872b77ce45d94fe78eae8
Adelani <adeoyejoel121@gmail.com>
2025-03-20 17:10:46 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,173 @@
+import {
+  c,
+  python,
+  java,
+  cpp,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  edunet,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  threejs,
+  mhft,
+  sketcher,
+  mathwork,
+  CompileVortex,
+  eduskill,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  { title: "C", icon: c },
+  { title: "C++", icon: cpp },
+  { title: "Python", icon: python },
+  { title: "Java", icon: java },
+];
+
+export const technologies = [
+  { name: "HTML 5", icon: html },
+  { name: "CSS 3", icon: css },
+  { name: "JavaScript", icon: javascript },
+  { name: "Rect JS", icon: reactjs },
+  { name: "Tailwind CSS", icon: tailwind },
+  { name: "Node JS", icon: nodejs },
+  { name: "Three JS", icon: threejs },
+  { name: "git", icon: git },
+];
+
+export const experiences = [
+  {
+    title: "Frontend Developer",
+    company_name: "Zety Ltd | AWS Academy | AICTE",
+    icon: eduskill,
+    iconBg: "#161329",
+    date: "Oct 2024 - March 2025",
+    points: [
+      "Acquired hands-on knowledge of AWS Cloud Foundation, delving into cloud infrastructure, services, and deployment.",
+      "also gaining a solid foundation in Machine Learning, covering topics like algorithms, data analysis, and model building.",
+      " Engaging in design and development of enterprise-level microservice applications in the Commerce Experience Group, driving $35.3 billion in annual revenue",
+    ],
+  },
+  {
+    title: "Software Developer",
+    company_name: "Next | Tech",
+    icon: mathwork,
+    iconBg: "#161329",
+    date: "Feb 2024 - Aug 2024",
+    points: [
+      " Implemented enterprise applications of Prime's Content Experiment Platforms using Next js, Java, React, AngularJS, AWS, and DynamoDB.",
+      " Implemented server-side rendering (SSR) and static site generation (SSG) using Next.js, optimizing website speed and user experience.",
+      "• Automated and optimized business logic for the core marketing experiments, including A/B, Auto-Targeting, and Multivariate Testing.",
+    ],
+  },
+  {
+    title: "Software Developer Intern",
+    company_name: "InnovaTech Ventures Foundation | IBM SkillsBuild",
+    icon: edunet,
+    iconBg: "#161329",
+    date: "Nov 2022 - May 2023",
+    points: [
+      "• Implemented server-side rendering (SSR) and static site generation (SSG) using Next.js, optimizing website speed and user experience.",
+      "Engineering owner of the E2E experience of the commerce platform's cancel, return, and exchange systems (PC/Mobile).",
+      "Engineered a comprehensive Mental Health Fitness Tracker ML model utilizing Python and scikit-learn.",
+      "Maximized the model's performance by refining model parameters and employing ensemble methods, yielding an outstanding accuracy percentage of 98.50%.",
+      "Leveraged 12 regression algorithms to attain precise outcomes in analyzing and predicting mental fitness levels across 150+ countries.",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    name: "WeatherPedia",
+    description:
+      "Web-based platform that allows users to access weather information for their location by entering it in the search field",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "HTML", color: "green-text-gradient" },
+      { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
+      { name: "Weather API by API Ninjas", color: "yellow-text-gradient" },
+    ],
+    image: weatherpedia,
+    source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
+  },
+  {
+    name: "Terminal Like Portfolio Website",
+    description:
+      "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
+    tags: [
+      { name: "HTML", color: "blue-text-gradient" },
+      { name: "css", color: "green-text-gradient" },
+      { name: "Javascript", color: "pink-text-gradient" },
+    ],
+    image: termpw,
+    source_code_link: "https://github.com/lohitkolluri/lohitkolluri.github.io",
+  },
+  {
+    name: "Mental Health Fitness Tracker",
+    description:
+      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Jupyter Notebook", color: "green-text-gradient" },
+      { name: "Regression Algorithms", color: "pink-text-gradient" },
+    ],
+    image: mhft,
+    source_code_link:
+      "https://github.com/lohitkolluri/mental_health_fitness_tracker",
+  },
+  {
+    name: "PayloadMaster",
+    description:
+      "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "shell", color: "blue-text-gradient" },
+    ],
+    image: payloadmaster,
+    source_code_link: "https://github.com/lohitkolluri/PayloadMaster",
+  },
+  {
+    name: "CompileVortex",
+    description:
+      "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "CSS", color: "green-text-gradient" },
+      { name: "HTML", color: "pink-text-gradient" },
+    ],
+    image: CompileVortex,
+    source_code_link: "https://github.com/lohitkolluri/CompileVortex",
+  },
+  {
+    name: "Sketcher",
+    description:
+      "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
+    tags: [
+      { name: "OpenCV", color: "blue-text-gradient" },
+      { name: "Matplotlib", color: "green-text-gradient" },
+      { name: "Python", color: "pink-text-gradient" },
+    ],
+
+    image: sketcher,
+    source_code_link: "https://github.com/lohitkolluri/Image_to_Pencil_Sketch_App",
+  },
+];
